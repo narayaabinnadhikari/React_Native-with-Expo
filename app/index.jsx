2e8177f5316c57d5e5a30572e8fca37076c478dc
naraabinnadhikari <narayanabin223@gmail.com>
2024-11-02 23:16:43 +0545

    4. List Views. menu screen added, safeareaview, platform identify, flatlist, navigation.

--- app/index.jsx
+++ app/index.jsx
@@ -10,6 +10,13 @@ const app = () => {
 
       <Text style={styles.title}>Hot Coffee Shop</Text>
 
+      <Link href="/menu" style={{ marginHorizontal: 'auto'}} asChild>
+      <Pressable style={styles.button} >
+      <Text style={styles.buttonText}>
+        Our Menu
+      </Text>
+      </Pressable>
+      </Link>
       <Link href="/contact" style={{ marginHorizontal: 'auto'}} asChild>
       <Pressable style={styles.button} >
       <Text style={styles.buttonText}>
@@ -56,10 +63,12 @@ const styles =StyleSheet.create({
   },
   button: {
     height: 60,
+    width: 150,
     borderRadius: 20,
     justifyContent: 'center',
     backgroundColor: 'rgba(0,0,0, 0.75)',
     padding: 6,
+    marginBottom: 50,
   },
 
   buttonText: {
